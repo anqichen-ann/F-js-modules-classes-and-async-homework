789a87dd1f43954c2ef6324cff8087f3ff651912
Anqi Chen <anqi.chen@AnqideMacBook-Pro.local>
2020-10-16 13:14:01 +0800

    [anqi] feat async homework

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,6 @@
+import getUrl from './utils';
+import * as info from './constant'
 
+export const url = getUrl.myURL(info.ADDRESS,info.PORT,info.PATH);
+
+export const region = info.REGION;
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,28 @@
-export const getPoetry
+export const getPoetry = async function(){
+    // let result = [];
+    // return fetch('https://v1.jinrishici.com/all.json', {
+    //     method: "GET",
+    //     headers: {
+    //       "accept": "text/html"
+    //     },
+    //     credentials: "same-origin"
+    //   }).then( data => data.json()
+    //       ).then(data => {
+    //       result.push(data.origin);
+    //       result.push(data.author);
+    //       result.push(data.content);
+    //       return result;
+    // }).catch(error => 
+    //     console.log(error))
+    try{
+        const prom1 = await fetch('https://v1.jinrishici.com/all.json');
+        const data = await prom1.json();
+        let result = [];
+        result.push(data.origin);
+        result.push(data.author);
+        result.push(data.content);
+        return result;
+    }catch(error){
+        console.log(error)
+    }
+}
